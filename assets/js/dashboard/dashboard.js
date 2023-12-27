89ac0b17031c72614d38fbde369d5b9bd68f94f8
JenniBURR <jennifer.mendoza2@carsu.edu.ph>
2023-12-27 21:27:30 +0800

    Attempted Fixes

--- assets/js/dashboard/dashboard.js
+++ assets/js/dashboard/dashboard.js
@@ -1,83 +1,92 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to fetch and display data for each table
-    function fetchTableData(endpoint, tableBodyId, limit) {
-      const url = new URL(endpoint);
-      url.searchParams.set('_limit', limit);
+  // Function to fetch and display data for each table
+  // Function to fetch and display data for each table
+function fetchTableData(endpoint, tableBodyId, limit) {
+  const url = new URL(endpoint);
+  url.searchParams.set('_limit', limit);
 
-      fetch(url)
-          .then(response => response.json())
-          .then(data => {
-              const tableBody = document.getElementById(tableBodyId);
-              tableBody.innerHTML = ''; // Clear existing rows
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          const tableBody = document.getElementById(tableBodyId);
+          tableBody.innerHTML = ''; // Clear existing rows
 
-              if (data.length > 0) {
-                  data.forEach(item => {
-                      const row = document.createElement('tr');
-                      // Adjust the row creation based on the actual structure of your data
-                      // Use the correct key names for the Supplier table
-                      row.innerHTML = `<td>${item.supplier_id}</td><td>${item.supplier_name}</td><td>${item.contact_info}</td>`;
-                      tableBody.appendChild(row);
-                  });
-              } else {
-                  // Display a message when no data is available
+          if (data.length > 0) {
+              data.forEach(item => {
                   const row = document.createElement('tr');
-                  row.innerHTML = `<td colspan="3">No data found</td>`;
+                  // Adjust the row creation based on the actual structure of your data
+                  // Use the correct key names for the Supplier table
+                  row.innerHTML = `<td>${item.supplier_id || item.medicine_id || item.inventory_id || item.stock_id}</td>
+                                  <td>${item.supplier_name || item.medicine_name || item.quantity || item.movement_type}</td>
+                                  <td>${item.contact_info || item.manufacturer || item.purchase_date || item.quantity}</td>`;
                   tableBody.appendChild(row);
-              }
-          })
-          .catch(error => console.error('Error fetching data:', error));
-  }
+              });
+          } else {
+              // Display a message when no data is available
+              const row = document.createElement('tr');
+              row.innerHTML = `<td colspan="3">No data found</td>`;
+              tableBody.appendChild(row);
+          }
+      })
+      .catch(error => console.error('Error fetching data:', error));
+}
 
   // Initial fetch when the page loads
   fetchTableData('http://127.0.0.1:8000/api/medicine', 'medicineTableBody', 5);
   fetchTableData('http://127.0.0.1:8000/api/supplier', 'supplierTableBody', 5);
   fetchTableData('http://127.0.0.1:8000/api/inventory', 'inventoryTableBody', 5);
   fetchTableData('http://127.0.0.1:8000/api/stock', 'stocksTableBody', 5);
+
+  // Add event listener to refresh the table when a search is submitted
+  document.querySelectorAll('.search-form').forEach(form => {
+      form.addEventListener('submit', function (event) {
+          event.preventDefault();
+          const endpoint = form.dataset.endpoint;
+          const tableBodyId = form.dataset.tableBody;
+          const searchTerm = form.search.value.trim();
+          const limit = form.dataset.limit;
+
+          const url = new URL(endpoint);
+          url.searchParams.set('search', searchTerm);
+
+          fetchTableData(url.toString(), tableBodyId, limit);
+      });
+  });
+
+  // Event listeners for table navigation buttons
+  document.getElementById('nextSupplier').addEventListener('click', function () {
+      handleTableNavigation('supplierTable', 'next');
+  });
+
+  document.getElementById('prevSupplier').addEventListener('click', function () {
+      handleTableNavigation('supplierTable', 'prev');
+  });
+
+  document.getElementById('nextInventory').addEventListener('click', function () {
+      handleTableNavigation('inventoryTable', 'next');
+  });
+
+  document.getElementById('prevInventory').addEventListener('click', function () {
+      handleTableNavigation('inventoryTable', 'prev');
+  });
+
+  document.getElementById('nextStocks').addEventListener('click', function () {
+      handleTableNavigation('stocksTable', 'next');
+  });
+
+  document.getElementById('prevStocks').addEventListener('click', function () {
+      handleTableNavigation('stocksTable', 'prev');
+  });
 });
 
-// Add event listener to refresh the table when a search is submitted
-document.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const form = event.target;
+// Add your existing code for fetching medicines, suppliers, inventory, and stocks here
 
-  if (form.classList.contains('search-form')) {
-      const endpoint = form.dataset.endpoint;
-      const tableBodyId = form.dataset.tableBody;
-      const searchTerm = form.search.value.trim();
+// Function to handle table navigation
+function handleTableNavigation(tableId, direction) {
+  // Implement logic for navigating through the table
+  console.log(`Navigating ${direction} in table ${tableId}`);
+}
 
-      const limit = form.dataset.limit;
-
-      const url = new URL(endpoint);
-      url.searchParams.set('search', searchTerm);
-
-      fetchTableData(url.toString(), tableBodyId, limit);
-  }
-
-    // Event listeners for table navigation buttons
-    document.getElementById('nextSupplier').addEventListener('click', function () {
-        handleTableNavigation('supplierTable', 'next');
-    });
-
-    document.getElementById('prevSupplier').addEventListener('click', function () {
-        handleTableNavigation('supplierTable', 'prev');
-    });
-
-    document.getElementById('nextInventory').addEventListener('click', function () {
-        handleTableNavigation('inventoryTable', 'next');
-    });
-
-    document.getElementById('prevInventory').addEventListener('click', function () {
-        handleTableNavigation('inventoryTable', 'prev');
-    });
-
-    document.getElementById('nextStocks').addEventListener('click', function () {
-        handleTableNavigation('stocksTable', 'next');
-    });
-
-    document.getElementById('prevStocks').addEventListener('click', function () {
-        handleTableNavigation('stocksTable', 'prev');
-    });
-});
 
 document.addEventListener('DOMContentLoaded', function () {
   // Function to fetch and display medicines
