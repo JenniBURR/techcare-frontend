87919e3bfdfd9090248b4405d7059564250a04d5
JenniBURR <jennifer.mendoza2@carsu.edu.ph>
2023-12-28 10:24:06 +0800

    Added CREATE Buttons

--- assets/js/inventory/inventory.js
+++ assets/js/inventory/inventory.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to fetch and display data for each table
+    // Fetch and display data for each table
     function fetchTableData(endpoint, tableBodyId, limit) {
         const url = new URL(endpoint);
         url.searchParams.set('_limit', limit);
@@ -13,15 +13,46 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (data.length > 0) {
                     data.forEach(item => {
                         const row = document.createElement('tr');
-                        row.innerHTML = `<td>${item.id}</td>
-                                    <td>${item.name || item.medicine_name || item.supplier_name || item.inventory_id}</td>
-                                    <td>${item.manufacturer || item.contact_info || item.branch_id || item.movement_type}</td>
-                                    <td>${item.expirydate || item.purchase_date || item.quantity || ''}</td>
-                                    <td>${item.quantity || item.price || ''}</td>
-                                    <td>
-                                        <button class="btn btn-warning btn-sm" onclick="editItem('${tableBodyId}', ${item.id})">Edit</button>
-                                        <button class="btn btn-danger btn-sm" onclick="deleteItem('${tableBodyId}', ${item.id})">Delete</button>
-                                    </td>`;
+                        let rowHTML = '';
+
+                        // Adjust the row creation based on the actual structure of your data
+                        switch (tableBodyId) {
+                            case 'medicineTableBody':
+                                rowHTML = `<td>${item.medicine_id}</td>
+                                            <td>${item.medicine_name}</td>
+                                            <td>${item.manufacturer}</td>
+                                            <td>${item.expirydate}</td>
+                                            <td>${item.quantity}</td>
+                                            <td>${item.price}</td>`;
+                                break;
+                            case 'supplierTableBody':
+                                rowHTML = `<td>${item.supplier_id}</td>
+                                            <td>${item.supplier_name}</td>
+                                            <td>${item.contact_info}</td>`;
+                                break;
+                            case 'inventoryTableBody':
+                                rowHTML = `<td>${item.inventory_id}</td>
+                                            <td>${item.medicine_id}</td>
+                                            <td>${item.supplier_id}</td>
+                                            <td>${item.quantity}</td>
+                                            <td>${item.purchase_date}</td>`;
+                                break;
+                            case 'stocksTableBody':
+                                rowHTML = `<td>${item.stock_id}</td>
+                                            <td>${item.inventory_id}</td>
+                                            <td>${item.branch_id}</td>
+                                            <td>${item.movement_type}</td>
+                                            <td>${item.quantity}</td>`;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        row.innerHTML = `${rowHTML}
+                                        <td>
+                                            <button class="btn btn-warning btn-sm" onclick="editItem('${tableBodyId}', ${item.id})">Edit</button>
+                                            <button class="btn btn-danger btn-sm" onclick="deleteItem('${tableBodyId}', ${item.id})">Delete</button>
+                                        </td>`;
                         tableBody.appendChild(row);
                     });
                 } else {
@@ -38,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
     fetchTableData('http://127.0.0.1:8000/api/medicine', 'medicineTableBody', 5);
     fetchTableData('http://127.0.0.1:8000/api/supplier', 'supplierTableBody', 5);
     fetchTableData('http://127.0.0.1:8000/api/inventory', 'inventoryTableBody', 5);
-    fetchTableData('http://127.0.0.1:8000/api/stock', 'stocksTableBody', 5);
+    fetchTableData('http://127.0.0.1:8000/api/stocks', 'stocksTableBody', 5);
 
     // Add event listener to refresh the table when a search is submitted
     document.querySelectorAll('.search-form').forEach(form => {
@@ -57,6 +88,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Event listeners for table navigation buttons
+    document.getElementById('nextMedicine').addEventListener('click', function () {
+        handleTableNavigation('medicineTable', 'next');
+    });
+
+    document.getElementById('prevMedicine').addEventListener('click', function () {
+        handleTableNavigation('medicineTable', 'prev');
+    });
+
     document.getElementById('nextSupplier').addEventListener('click', function () {
         handleTableNavigation('supplierTable', 'next');
     });
@@ -81,179 +120,139 @@ document.addEventListener('DOMContentLoaded', function () {
         handleTableNavigation('stocksTable', 'prev');
     });
 
-    document.addEventListener('DOMContentLoaded', function () {
-        // Function to fetch and display medicines
-        function fetchMedicines() {
-            fetch('http://127.0.0.1:8000/api/medicine')
-                .then(response => response.json())
-                .then(data => {
-                    const medicineTableBody = document.getElementById('medicineTableBody');
-                    medicineTableBody.innerHTML = ''; // Clear existing rows
-      
-                    if (data.length > 0) {
-                        data.forEach(medicine => {
-                            const row = document.createElement('tr');
-                            row.innerHTML = `
-                                <td>${medicine.medicine_id}</td>
-                                <td>${medicine.medicine_name}</td>
-                                <td>${medicine.manufacturer}</td>
-                                <td>${medicine.expirydate}</td>
-                                <td>${medicine.quantity}</td>
-                                <td>${medicine.price}</td>
-                            `;
-                            medicineTableBody.appendChild(row);
-                        });
-                    } else {
-                        // Display a message when no medicines are available
-                        const row = document.createElement('tr');
-                        row.innerHTML = '<td colspan="6">No medicines found</td>';
-                        medicineTableBody.appendChild(row);
-                    }
-                })
-                .catch(error => console.error('Error fetching medicines:', error));
-        }
-      
-        // Initial fetch when the page loads
-        fetchMedicines();
-      });
-      
-      document.addEventListener('DOMContentLoaded', function () {
-        // Function to fetch and display suppliers
-        function fetchSuppliers() {
-            fetch('http://127.0.0.1:8000/api/supplier')
-                .then(response => response.json())
-                .then(data => {
-                    const suppliersTableBody = document.getElementById('suppliersTableBody');
-                    suppliersTableBody.innerHTML = ''; // Clear existing rows
-      
-                    if (data.length > 0) {
-                        data.forEach(item => {
-                            const row = document.createElement('tr');
-                            row.innerHTML = `
-                                <td>${item.supplier_id}</td>
-                                <td>${item.supplier_name}</td>
-                                <td>${item.contact_info}</td>
-                            `;
-                            suppliersTableBody.appendChild(row);
-                        });
-                    } else {
-                        // Display a message when no suppliers are available
-                        const row = document.createElement('tr');
-                        row.innerHTML = '<td colspan="3">No suppliers found</td>';
-                        suppliersTableBody.appendChild(row);
-                    }
-                })
-                .catch(error => console.error('Error fetching suppliers:', error));
-        }
-      
-        // Initial fetch when the page loads
-        fetchSuppliers();
-      });
-      
-      document.addEventListener('DOMContentLoaded', function () {
-        // Function to fetch and display inventory
-        function fetchInventory() {
-            fetch('http://127.0.0.1:8000/api/inventory')
-                .then(response => response.json())
-                .then(data => {
-                    const inventoryTableBody = document.getElementById('inventoryTableBody');
-                    inventoryTableBody.innerHTML = ''; // Clear existing rows
-      
-                    if (data.length > 0) {
-                        data.forEach(item => {
-                            const row = document.createElement('tr');
-                            row.innerHTML = `
-                                <td>${item.inventory_id}</td>
-                                <td>${item.medicine_id}</td>
-                                <td>${item.supplier_id}</td>
-                                <td>${item.quantity}</td>
-                                <td>${item.purchase_date}</td>
-                            `;
-                            inventoryTableBody.appendChild(row);
-                        });
-                    } else {
-                        // Display a message when no inventory items are available
-                        const row = document.createElement('tr');
-                        row.innerHTML = '<td colspan="6">No inventory items found</td>';
-                        inventoryTableBody.appendChild(row);
-                    }
-                })
-                .catch(error => console.error('Error fetching inventory:', error));
-        }
-      
-        // Initial fetch when the page loads
-        fetchInventory();
-      });
-      
-      document.addEventListener('DOMContentLoaded', function () {
-        // Function to fetch and display inventory
-        function fetchStock() {
-            fetch('http://127.0.0.1:8000/api/inventory')
-                .then(response => response.json())
-                .then(data => {
-                    const stockTableBody = document.getElementById('stockTableBody');
-                    stockTableBody.innerHTML = ''; // Clear existing rows
-      
-                    if (data.length > 0) {
-                        data.forEach(item => {
-                            const row = document.createElement('tr');
-                            row.innerHTML = `
-                                <td>${item.stock_id}</td>
-                                <td>${item.inventory_id}</td>
-                                <td>${item.branch_id}</td>
-                                <td>${item.movement_type}</td>
-                                <td>${item.quantity}</td>
-                            `;
-                            stockTableBody.appendChild(row);
-                        });
-                    } else {
-                        // Display a message when no inventory items are available
-                        const row = document.createElement('tr');
-                        row.innerHTML = '<td colspan="6">No stock items found</td>';
-                        inventoryTableBody.appendChild(row);
-                    }
-                })
-                .catch(error => console.error('Error fetching stocks:', error));
-        }
-      
-        // Initial fetch when the page loads
-        fetchStock();
-      });
-      
-
     // Function to handle table navigation
     function handleTableNavigation(tableId, direction) {
         // Implement logic for navigating through the table
         console.log(`Navigating ${direction} in table ${tableId}`);
     }
 
+    // Function to handle adding a new item to the table
+    function addNewItem(endpoint, newItem, tableBodyId) {
+        fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newItem),
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Assuming the response includes the newly added item
+                const tableBody = document.getElementById(tableBodyId);
+                const row = document.createElement('tr');
+                let rowHTML = '';
+
+                // Adjust the row creation based on the actual structure of your data
+                switch (tableBodyId) {
+                    case 'medicineTableBody':
+                        rowHTML = `<td>${data.medicine_id}</td>
+                                    <td>${data.medicine_name}</td>
+                                    <td>${data.manufacturer}</td>
+                                    <td>${data.expirydate}</td>
+                                    <td>${data.quantity}</td>
+                                    <td>${data.price}</td>`;
+                        break;
+                    case 'supplierTableBody':
+                        rowHTML = `<td>${data.supplier_id}</td>
+                                    <td>${data.supplier_name}</td>
+                                    <td>${data.contact_info}</td>`;
+                        break;
+                    case 'inventoryTableBody':
+                        rowHTML = `<td>${data.inventory_id}</td>
+                                    <td>${data.medicine_id}</td>
+                                    <td>${data.supplier_id}</td>
+                                    <td>${data.quantity}</td>
+                                    <td>${data.purchase_date}</td>`;
+                        break;
+                    case 'stocksTableBody':
+                        rowHTML = `<td>${data.stock_id}</td>
+                                    <td>${data.inventory_id}</td>
+                                    <td>${data.branch_id}</td>
+                                    <td>${data.movement_type}</td>
+                                    <td>${data.quantity}</td>`;
+                        break;
+                    default:
+                        break;
+                }
+
+                row.innerHTML = `${rowHTML}
+                                <td>
+                                    <button class="btn btn-warning btn-sm" onclick="editItem('${tableBodyId}', ${data.id})">Edit</button>
+                                    <button class="btn btn-danger btn-sm" onclick="deleteItem('${tableBodyId}', ${data.id})">Delete</button>
+                                </td>`;
+                tableBody.appendChild(row);
+
+                // Clear the form fields
+                document.getElementById(`add${tableBodyId}Form`).reset();
+            })
+            .catch(error => console.error('Error adding new item:', error));
+    }
+
     // Function to handle editing an item
     window.editItem = function (tableBodyId, itemId) {
-        console.log(`Editing item with ID ${itemId} in table ${tableBodyId}`);
-        // Implement logic to show a modal or redirect to the edit page for the specific item
-        // Example: window.location.href = `/edit-item.html?id=${itemId}&table=${tableBodyId}`;
+        // You can implement the edit functionality here, e.g., open a modal or redirect to an edit page
+        console.log(`Edit item with ID ${itemId} in table ${tableBodyId}`);
     };
 
     // Function to handle deleting an item
     window.deleteItem = function (tableBodyId, itemId) {
-        console.log(`Deleting item with ID ${itemId} in table ${tableBodyId}`);
-        // Implement logic to confirm deletion and make an API request to delete the item
-        const deleteConfirmed = confirm(`Are you sure you want to delete this item with ID ${itemId}?`);
+        // You can implement the delete functionality here, e.g., show a confirmation modal
+        console.log(`Delete item with ID ${itemId} in table ${tableBodyId}`);
+    }
 
-        if (deleteConfirmed) {
-            // Assuming you have an API endpoint like '/api/items/:id' for deleting
-            fetch(`http://127.0.0.1:8000/api/${tableBodyId}/${itemId}`, {
-                method: 'DELETE'
-            })
-                .then(response => {
-                    if (response.ok) {
-                        // Item deleted successfully, refresh the table
-                        fetchTableData(`http://127.0.0.1:8000/api/${tableBodyId}`, tableBodyId, 5);
-                    } else {
-                        console.error(`Error deleting item with ID ${itemId} from table ${tableBodyId}:`, response.statusText);
-                    }
-                })
-                .catch(error => console.error(`Error deleting item with ID ${itemId} from table ${tableBodyId}:`, error));
-        }
-    };
+    // Event listeners for adding new items
+    document.getElementById('addMedicineForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+
+        const newMedicineItem = {
+            medicine_name: formData.get('medicine_name'),
+            manufacturer: formData.get('manufacturer'),
+            expirydate: formData.get('expirydate'),
+            quantity: formData.get('quantity'),
+            price: formData.get('price'),
+        };
+
+        addNewItem('http://127.0.0.1:8000/api/medicine', newMedicineItem, 'medicineTableBody');
+    });
+
+    document.getElementById('addSupplierForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+
+        const newSupplierItem = {
+            supplier_name: formData.get('supplier_name'),
+            contact_info: formData.get('contact_info'),
+        };
+
+        addNewItem('http://127.0.0.1:8000/api/supplier', newSupplierItem, 'supplierTableBody');
+    });
+
+    document.getElementById('addInventoryForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+
+        const newInventoryItem = {
+            medicine_id: formData.get('medicine_id'),
+            supplier_id: formData.get('supplier_id'),
+            quantity: formData.get('quantity'),
+            purchase_date: formData.get('purchase_date'),
+        };
+
+        addNewItem('http://127.0.0.1:8000/api/inventory', newInventoryItem, 'inventoryTableBody');
+    });
+
+    document.getElementById('addStocksForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const formData = new FormData(this);
+
+        const newStocksItem = {
+            inventory_id: formData.get('inventory_id'),
+            branch_id: formData.get('branch_id'),
+            movement_type: formData.get('movement_type'),
+            quantity: formData.get('quantity'),
+        };
+
+        addNewItem('http://127.0.0.1:8000/api/stocks', newStocksItem, 'stocksTableBody');
+    });
 });
